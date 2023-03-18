b43ebac47a15d8a28758f1029eab2db44c29360f
Brancen Gregory <brancengregory@gmail.com>
2023-03-18 02:11:56 -0400

    WIP

--- build/cart.js
+++
@@ -1,8 +0,0 @@
-const CART_KEY = "cart";
-function getCart() {
-  const cart = localStorage.getItem(CART_KEY);
-  return cart ? JSON.parse(cart) : [];
-}
-function saveCart(cart) {
-  localStorage.setItem(CART_KEY, JSON.stringify(cart));
-}
