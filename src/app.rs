68aff67fa9df41dd693ac3c34b8bdde1cfdebfd7
Brancen Gregory <brancengregory@gmail.com>
2024-06-23 13:40:56 -0500

    WIP

--- src/app.rs
+++ src/app.rs
@@ -1,16 +1,16 @@
+use crate::cart::CartPage;
 use crate::error_template::{AppError, ErrorTemplate};
+use crate::product::{NoProduct, ProductProfile, Products};
 use leptos::*;
 use leptos_meta::*;
 use leptos_router::*;
-use crate::cart::CartPage;
-use crate::product::{NoProduct, Products, ProductProfile};
 
 #[component]
-pub fn App() -> impl IntoView {
+pub fn App(cx: Scope) -> impl IntoView {
     // Provides context that manages stylesheets, titles, meta tags, etc.
     provide_meta_context();
 
-    view! {
+    view! { cx,
         // injects a stylesheet into the document <head>
         // id=leptos means cargo-leptos will hot-reload this stylesheet
         <Stylesheet id="leptos" href="/pkg/andriy-store.css"/>
@@ -34,10 +34,10 @@ pub fn App() -> impl IntoView {
                 <Routes>
                     <Route path="/" view=HomePage/>
                     <Route path="/cart" view=CartPage/>
-                    <Route path="/products" view=Products>
-                      <Route path=":id" view=ProductProfile/>
-                      <Route path="" view=NoProduct/>
-                    </Route>
+                    <Route path="/products/:id" view=move |cx| {
+                      let params = use_params();
+                      view! { cx, <ProductProfile id={params.get("id")} /> }
+                    }/>
                 </Routes>
             </main>
         </Router>
@@ -46,63 +46,63 @@ pub fn App() -> impl IntoView {
 
 #[component]
 fn Navbar() -> impl IntoView {
-  view! {
-    <div class="navbar bg-base-100">
-      <div class="navbar-start">
-        <div class="dropdown">
-          <div tabindex="0" role="button" class="btn btn-ghost btn-circle md:hidden">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-5 h-5 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+    view! {
+      <div class="navbar bg-base-100">
+        <div class="navbar-start">
+          <div class="dropdown">
+            <div tabindex="0" role="button" class="btn btn-ghost btn-circle md:hidden">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-5 h-5 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+            </div>
+            <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+              <li><a href="/">"Home"</a></li>
+              <li>
+                <a href="/products">"Products"</a>
+                <ul class="p-2">
+                  <li><a href="/products#caketoppers">"Cake Toppers"</a></li>
+                  <li><a href="/products#ornaments">"Ornaments"</a></li>
+                </ul>
+              </li>
+              <li><a href="/cart">"Cart"</a></li>
+            </ul>
           </div>
-          <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+          <a href="/" class="btn btn-ghost text-xl">"Andriy's Store"</a>
+        </div>
+        <div class="navbar-center hidden md:flex">
+          <ul class="menu menu-horizontal px-1">
             <li><a href="/">"Home"</a></li>
             <li>
-              <a href="/products">"Products"</a>
-              <ul class="p-2">
-                <li><a href="/products#caketoppers">"Cake Toppers"</a></li>
-                <li><a href="/products#ornaments">"Ornaments"</a></li>
-              </ul>
+              <details>
+                <summary>"Products"</summary>
+                <ul class="p-2">
+                  <li><a href="/products#caketoppers">"Cake Toppers"</a></li>
+                  <li><a href="/products#ornaments">"Ornaments"</a></li>
+                </ul>
+              </details>
             </li>
             <li><a href="/cart">"Cart"</a></li>
           </ul>
         </div>
-        <a href="/" class="btn btn-ghost text-xl">"Andriy's Store"</a>
-      </div>
-      <div class="navbar-center hidden md:flex">
-        <ul class="menu menu-horizontal px-1">
-          <li><a href="/">"Home"</a></li>
-          <li>
-            <details>
-              <summary>"Products"</summary>
-              <ul class="p-2">
-                <li><a href="/products#caketoppers">"Cake Toppers"</a></li>
-                <li><a href="/products#ornaments">"Ornaments"</a></li>
-              </ul>
-            </details>
-          </li>
-          <li><a href="/cart">"Cart"</a></li>
-        </ul>
-      </div>
-      <div class="navbar-end">
-        <div class="dropdown dropdown-end">
-          <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
-            <div class="indicator">
-              <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
-              <span class="badge badge-sm indicator-item">8</span>
+        <div class="navbar-end">
+          <div class="dropdown dropdown-end">
+            <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
+              <div class="indicator">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
+                <span class="badge badge-sm indicator-item">8</span>
+              </div>
             </div>
-          </div>
-          <div tabindex="0" class="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow">
-            <div class="card-body">
-              <span class="font-bold text-lg">8 Items</span>
-              <span class="text-info">Subtotal: $999</span>
-              <div class="card-actions">
-                <a href="/cart" class="btn btn-primary btn-block">View cart</a>
+            <div tabindex="0" class="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow">
+              <div class="card-body">
+                <span class="font-bold text-lg">8 Items</span>
+                <span class="text-info">Subtotal: $999</span>
+                <div class="card-actions">
+                  <a href="/cart" class="btn btn-primary btn-block">View cart</a>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  }
+    }
 }
 
 /// Renders the home page of your application.
--- src/product.rs
+++ src/product.rs
@@ -1,29 +1,74 @@
+use std::fmt::{Display, Formatter, Result};
+
 use leptos::*;
 use leptos_router::*;
 
+pub struct Price(f64);
+
+impl Price {
+  pub fn new(value: f64) -> Self {
+    Price(value)
+  }
+}
+
+impl Display for Price {
+  fn fmt(&self, f: &mut Formatter) -> Result {
+    write!(f, "{:.2}", self.0)
+  }
+}
+
 struct Product {
-  id: i32,
-  name: String,
-  description: String
+    id: String,
+    name: String,
+    description: String,
+    image: String,
+    price: Price,
+}
+
+fn get_product_by_id(id: String) -> Option<Product> {
+  let products = vec![
+    Product {
+      id: "1".to_string(),
+      name: "".to_string(),
+      description: "".to_string(),
+      image: "public/assets/christmas-ornament.jpg".to_string(),
+      price: Price::new(49.99),
+    }
+  ];
+
+  products.into_iter().find(|p| p.id == id)
 }
 
 #[component]
 pub fn Products() -> impl IntoView {
-  view! {
-    "Product Page muh'fuckah"
-  }
+    view! {
+      "Product Page muh'fuckah"
+    }
 }
 
 #[component]
-pub fn ProductProfile() -> impl IntoView {
-  view! {
-    "Product Details Page"
+pub fn ProductProfile(id: String) -> impl IntoView {
+  if let Some(product) = get_product_by_id(id) {
+    view! {
+      <div>
+        <img src={product.image.clone()} alt={product.name.clone()} />
+        <h2>{product.name.clone()}</h2>
+        <p>{product.description.clone()}</p>
+        <p>{format!("${:.2}", product.price)}</p>
+      </div>
+    }
+  } else {
+    view! {
+      <div class="product-not-found">
+        <p>"Product not found."</p>
+      </div>
+    }
   }
 }
 
 #[component]
 pub fn NoProduct() -> impl IntoView {
-  view! {
-    "No products :'("
-  }
+    view! {
+      "No products :'("
+    }
 }
